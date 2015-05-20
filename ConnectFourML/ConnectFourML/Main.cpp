a95f17a7ba586abcb0b226fc08143cc2340fa5c2
QuickSandhu <raymonsandhu@yahoo.com>
2015-05-20 12:28:42 +0200

    nothing important

--- ConnectFourML/ConnectFourML/Main.cpp
+++ ConnectFourML/ConnectFourML/Main.cpp
@@ -15,17 +15,6 @@ int main(int argc, char* argv[])
 			showBoard = true;
 	}
 
-#pragma region test trainSetToStringy
-	vector<TrainPair> trainSet;
-	bool input[84] = { 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-		0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-		0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, };
-	trainSet.push_back(TrainPair(input, 0.98));
-	std::string str;
-	trainSetToString(trainSet, str);
-	std::cout << str;
-#pragma endregion
 	//ConnectFour();
 	system("pause");
 	return 0;
